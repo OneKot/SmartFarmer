da25373b5bd3d4a5b1ffa54c69b99e4d527f4e62
OneKot <itmobileprogrammer@gmail.com>
2024-11-30 19:01:31 +0300

    test

--- app/src/main/java/com/example/smartfarmer/MainActivity.java
+++ app/src/main/java/com/example/smartfarmer/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.smartfarmer;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,7 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView1;
+    TextView textView1,textView2;
+    EditText editText1,editText2,editText3;
+    Button button1,button2;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        editText1 = findViewById(R.id.editTextText);
+        editText2 = findViewById(R.id.editTextTextPassword1);
+        editText3 = findViewById(R.id.editTextTextPassword2);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/smartfarmer/MainActivity2.java
+++ app/src/main/java/com/example/smartfarmer/MainActivity2.java
@@ -1,6 +1,11 @@
 package com.example.smartfarmer;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    EditText editText1,editText2;
+    Button button1,button2;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,19 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editText1 = findViewById(R.id.editTextText_s);
+        editText2 = findViewById(R.id.editTextTextPassword_s);
+        button1 = findViewById(R.id.button1_s);
+        button2 = findViewById(R.id.button2_s);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
