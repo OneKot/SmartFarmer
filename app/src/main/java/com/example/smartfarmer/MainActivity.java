d9c6edc7e13e36d3ccedf1b500ddf0cad270869f
OneKot <itmobileprogrammer@gmail.com>
2024-11-30 20:05:10 +0300

    test

--- app/src/main/java/com/example/smartfarmer/MainActivity.java
+++ app/src/main/java/com/example/smartfarmer/MainActivity.java
@@ -7,14 +7,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+
     TextView textView1,textView2;
     EditText editText1,editText2,editText3;
     Button button1,button2;
@@ -31,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        auth = FirebaseAuth.getInstance();
         textView1 = findViewById(R.id.textView1);
         textView2 = findViewById(R.id.textView2);
         editText1 = findViewById(R.id.editTextText);
@@ -39,6 +49,35 @@ public class MainActivity extends AppCompatActivity {
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = editText1.getText().toString().trim();
+                String pass = editText2.getText().toString().trim();
+
+                if(user.isEmpty()){
+                        editText1.setError("Поле e-mail не может быть пустым");
+                }
+                if(pass.isEmpty()){
+                    editText2.setError("Поле Пароль не может быть пустым");
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Успешная регистрация", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+                                startActivity(intent);
+                            }else {
+                                Toast.makeText(MainActivity.this, "Не удалось зарегистрироваться"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,5 +85,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
--- app/src/main/java/com/example/smartfarmer/MainActivity2.java
+++ app/src/main/java/com/example/smartfarmer/MainActivity2.java
@@ -3,17 +3,27 @@ package com.example.smartfarmer;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity2 extends AppCompatActivity {
+    private FirebaseAuth auth;
     EditText editText1,editText2;
     Button button1,button2;
     @SuppressLint("MissingInflatedId")
@@ -28,10 +38,43 @@ public class MainActivity2 extends AppCompatActivity {
             return insets;
         });
 
+        auth = FirebaseAuth.getInstance();
         editText1 = findViewById(R.id.editTextText_s);
         editText2 = findViewById(R.id.editTextTextPassword_s);
         button1 = findViewById(R.id.button1_s);
         button2 = findViewById(R.id.button2_s);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editText1.getText().toString();
+                String pass = editText2.getText().toString();
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if (!pass.isEmpty()){
+                        auth.signInWithEmailAndPassword(email,pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(MainActivity2.this, "Успешный вход", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity2.this, "Не удалось войти", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }else{
+                        editText2.setError("Поле Пароль не может быть пустым");
+                    }
+                }else if(email.isEmpty()){
+                    editText1.setError("Поле e-mail не может быть пустым");
+                }else {
+                    editText1.setError("Пожалуйста, введите действительный e-mail");
+                }
+            }
+        });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +83,5 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 }
